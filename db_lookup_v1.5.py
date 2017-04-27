4578696aacd613621af2c7d4377c2d83b82cd912
Chris Poole <dyus@inclusiveprogress.com>
2017-04-26 21:23:16 -0700

    Update db_lookup_v1.5.py

--- db_lookup_v1.5.py
+++ db_lookup_v1.5.py
@@ -10,10 +10,10 @@ if len(sys.argv) < 2:
     sys.exit("\033[37mUsage: python "+sys.argv[0]+" <database.sql> <string_to_search>")
     
 print '''\033[32m
-| --:  Please enter username/email/password or any  :-- |
-| --:  string you want to look for.                 :-- |
+| --:          Simple database lookup          :-- |
+| --:   github://codingplanets/DatabaseLookup  :-- |
 \033[37m'''
-print("\033[31m| --:  Devloped by Chris Poole | @codingplanets     :-- |\033[37m")
+print("\033[31m| --:        Developed by Chris Poole          :-- |\033[37m")
 
 string = str(sys.argv[2])
 
