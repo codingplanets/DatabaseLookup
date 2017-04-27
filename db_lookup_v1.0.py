fbbbd41c6e5123efe29b472c35f7e4e481e2a541
Chris Poole <dyus@inclusiveprogress.com>
2017-04-26 20:03:54 -0700

    Create db_lookup_v1.0.py

---
+++ db_lookup_v1.0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#|-------------------------------------------|#
+#|  Usage: python db_lookup.py database.sql  |#
+#|  Simple database lookup | by Chris Poole  |#
+#|-------------------------------------------|#
+
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("\033[37mUsage: python "+sys.argv[0]+" [database.sql]")
+    
+print '''\033[32m
+| --:  Please enter username/email/password or any  :-- |
+| --:  string you want to look for.                 :-- |
+\033[37m'''
+print("\033[31m| --:  Devloped by Chris Poole | @codingplanets     :-- |\033[37m")
+
+string = raw_input("String To Search: ")
+
+with open(sys.argv[1], 'r') as data:
+    for grep in data:
+        if string in grep:
+			print grep
